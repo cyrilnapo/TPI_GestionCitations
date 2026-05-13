4305ba4ebd7535a765a5f1b38e0c7ada4997d8ca
cyrilnapo <napoleone.cyril@gmail.com>
2026-05-13 15:26:05 +0200

    feat: movies CRUD

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +19,20 @@ Route::middleware('web')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
+    // Public movies routes
+    Route::get('/movies', [MovieController::class, 'index']);
+    Route::get('/movies/{movie}', [MovieController::class, 'show']);
+
+    // Authenticated movies routes
     Route::middleware('auth:web')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user/me', [AuthController::class, 'me']);
+
+        Route::post('/movies', [MovieController::class, 'store']);
+        Route::put('/movies/{movie}', [MovieController::class, 'update']);
+        Route::patch('/movies/{movie}', [MovieController::class, 'update']);
+        Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
     });
 
-    
+
 });
