e230e417b0639f61418487b1e301e3d2a0b34bee
cyrilnapo <napoleone.cyril@gmail.com>
2026-05-18 13:26:17 +0200

    chore: handle error 404 and 500 in json properly

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,12 +6,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -26,6 +28,27 @@ return Application::configure(basePath: dirname(__DIR__))
                     'message' => 'You are not authenticated.',
                 ], 401);
             }
+
+            return null;
+        });
+
+        $exceptions->render(function (ModelNotFoundException $exception, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Resource not found',
+                ], 404);
+            }
+
+            return null;
+        });
+
+        $exceptions->render(function (Throwable $exception, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Server error',
+                ], 500);
+            }
+
             return null;
         });
     })->create();
