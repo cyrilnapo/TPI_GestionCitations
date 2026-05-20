c25829b152d46df0af9f4baa18642de94c307395
cyrilnapo <napoleone.cyril@gmail.com>
2026-05-20 14:19:17 +0200

    feat: sidebar navigation

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,22 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   imports: [
+    NgIf,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatDividerModule
+    RouterLinkActive
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  isAuthenticated = true;
+  isAdmin = false;
+}
